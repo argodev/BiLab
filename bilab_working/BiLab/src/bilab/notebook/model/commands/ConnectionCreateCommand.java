fe07a895de38109e672e3fa7a54187ed66b03b77
Rob Gillen <argodev@gmail.com>
2011-02-18 10:04:44 -0500

    Adding more copyright notices

--- bilab_working/BiLab/src/bilab/notebook/model/commands/ConnectionCreateCommand.java
+++ bilab_working/BiLab/src/bilab/notebook/model/commands/ConnectionCreateCommand.java
@@ -1,13 +1,36 @@
+/**
+* This document is a part of the source code and related artifacts for BiLab, an open source interactive workbench for 
+* computational biologists.
+*
+* http://computing.ornl.gov/
+*
+* Copyright Â© 2011 Oak Ridge National Laboratory
+*
+* This program is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General 
+* Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any 
+* later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more 
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License along with this program; if not, write to 
+* the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+* The license is also available at: http://www.gnu.org/copyleft/lgpl.html
+*/
+
+
 /*******************************************************************************
  * Copyright (c) 2004 Elias Volanakis.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Common Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
- *
- * Contributors:
- *    Elias Volanakis - initial API and implementation
- *******************************************************************************/
+ï¿½* All rights reserved. This program and the accompanying materials
+ï¿½* are made available under the terms of the Common Public License v1.0
+ï¿½* which accompanies this distribution, and is available at
+ï¿½* http://www.eclipse.org/legal/cpl-v10.html
+ï¿½*
+ï¿½* Contributors:
+ï¿½*ï¿½ï¿½ï¿½ï¿½Elias Volanakis - initial API and implementation
+ï¿½*******************************************************************************/
 package bilab.notebook.model.commands;
 
 import java.util.Iterator;
